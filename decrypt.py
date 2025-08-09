63df08762782718362004a37ffc52cc8ed9dd49e
Yuna Soo <yunasoo808@gmail.com>
2025-08-08 23:21:10 -1000

    created problem 3-decryption and verifying

---
+++ decrypt.py
@@ -0,0 +1,31 @@
+import hashlib # provides hash functions, SHA-256
+
+# convert into bytes then hash them then convert to integers
+def hashMessage(message):
+    messageBytes = message.encode('utf-8') #turn string into bytes
+    hashBytes = hashlib.sha256(messageBytes).digest() # compute SHA-256 hash
+    return int.from_bytes(hashBytes, byteorder='big') # convert hash into big integer
+    
+# decrypt signature using sender's public key (n, e)
+def decrypt(signature, e, n):
+    return pow(signature, e, n) # C^e mod n
+    
+# verify signature, rehash original message ourselves
+def verify(message, signature, e, n):
+    messageHash = hashMessage(message)
+    decryptedHash = decrypt(signature, e, n) # decrypt signature to get orignal hashMessage
+    if messageHash == decryptedHash:
+        return True # authentic
+    else:
+        return False # not authentic
+
+# Test cases, I used random numbers since problem 3 code was not done yet
+publicKeyN = 41541548417896984719696796324813
+publicKeyE = 978162364857
+signature = 54687953687541584791486977896
+message = "Hello World!"
+
+if verify(message, signature, publicKeyE, publicKeyN):
+    print("Message is authentic and untampered.")
+else:
+    print("Message is NOT authentic.")
